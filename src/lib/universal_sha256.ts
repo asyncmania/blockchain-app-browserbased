126969e3da5ec1fb70f9d39836aa2b8119a787a2
jilesanmi <ilesanmi.josiah@gmail.com>
2021-03-22 13:40:08 +0100

    crypto wrapper libray for sha 256

--- src/lib/universal_sha256.ts
+++ src/lib/universal_sha256.ts
@@ -4,3 +4,13 @@ function sha256_node(data: string): Promise<string> {
 }
 
 
+async function sha256_browser(data: string): Promise<string> {
+  const msgUnitArray = new TextEncoder().encode(data)
+  const hashByteArray = await crypto.subtle.digest('SHA-256', msgUnitArray)
+  const hashArray = Array.from(new Uint8Array(hashByteArray))
+  const hashHex = hashArray.map(b => '00'+ b.toString(16)).slice(-2).join('')
+  return hashHex
+}
+
+
+export const sha256 = typeof window === "undefined" ? sha256_node : sha256_browser
