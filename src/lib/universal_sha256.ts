abbab9af9eaa91a24b2087f4b61c90409acd2ddf
jilesanmi <ilesanmi.josiah@gmail.com>
2021-03-22 20:23:13 +0100

    creating the block and block chain classed

--- src/lib/universal_sha256.ts
+++ src/lib/universal_sha256.ts
@@ -8,9 +8,10 @@ async function sha256_browser(data: string): Promise<string> {
   const msgUnitArray = new TextEncoder().encode(data)
   const hashByteArray = await crypto.subtle.digest('SHA-256', msgUnitArray)
   const hashArray = Array.from(new Uint8Array(hashByteArray))
-  const hashHex = hashArray.map(b => '00'+ b.toString(16)).slice(-2).join('')
+  const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('')
   return hashHex
 }
 
 
 export const sha256 = typeof window === "undefined" ? sha256_node : sha256_browser
+
